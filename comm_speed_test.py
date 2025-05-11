4fa3ea9829c9b76d3acb5ce301b21c8acdc0a4c7
Bill Tubbs <bill.tubbs@me.com>
2025-05-10 21:24:49 -0700

    Test speed logging.

--- comm_speed_test.py
+++ comm_speed_test.py
@@ -1,9 +1,10 @@
-from collections import deque
+from itertools import cycle
 import numpy as np
 import serial
 from serial_comm.serial_comm import (send_data_to_arduino, receive_data_from_arduino)
 import logging
 import os
+import time
 
 
 logger = logging.getLogger(__name__)
@@ -26,29 +27,59 @@ def connect(address="/dev/tty.usbmodem112977801", baud=57600):
 def run_test(ser):
 
     test_data = [
-        (1, np.arange(0, 4794, dtype="uint8")),
-        (2, np.arange(0, 4794, dtype="uint8")),
-        (3, np.random.randint(256, size=4794, dtype="uint8")),
-        (4, np.random.randint(256, size=4794, dtype="uint8"))
+        (1, np.tile(np.arange(256, dtype="uint8"), 18)),
+        (2, np.tile(np.arange(255, -1, -1, dtype="uint8"), 18)),
+        (3, np.random.randint(256, size=4608, dtype="uint8")),
+        (4, np.random.randint(256, size=4608, dtype="uint8"))
     ]
 
     # Calculate check-sums to check data transmission
-    test_data = deque([(i, data, np.sum(data)) for i, data in test_data])
+    test_data_cycle = cycle([(i, data, np.sum(data)) for i, data in test_data])
+
+    # Wait for the initial hello message from the Arduino
+    hello_message = b'My name is '
+    t0 = time.time()
+    timeout_time = 10
+    while (time.time() - t0) < timeout_time:
+        if ser.in_waiting > 0:
+            num_bytes, data_received = receive_data_from_arduino(ser)
+            if num_bytes == 0:
+                bytes_received = bytes(data_received)
+                assert bytes_received.startswith(hello_message)
+                worker_name = bytes_received.removeprefix(
+                    hello_message
+                ).decode('utf')
+                logger.info(f"Hello from: {worker_name}")
+                break
+            else:
+                raise Exception("No hello message before data received")
+
+    if (time.time() - t0) > timeout_time:
+        raise Exception("No hello message received")
+
+    time.sleep(1.0)
+    t_start = time.time()
+    logger.info("Test start")
 
     waiting_for_response = False
-    while True:
+    n_iter = 100
+    i_iter = 0
+    while i_iter < n_iter:
 
         if ser.in_waiting == 0 and waiting_for_response is False:
             try:
-                i, data, check_sum = test_data.popleft()
+                i, data, check_sum = next(test_data_cycle)
             except IndexError:
                 break
+            logger.info(f"Sending Test {i} data...")
             send_data_to_arduino(ser, data)
+            logger.info("Data sent.")
             waiting_for_response = True
-            logger.info(f"Test {i} data sent.")
 
         if ser.in_waiting > 0:
+            logger.info("Receiving data...")
             num_bytes, data_received = receive_data_from_arduino(ser)
+            logger.info("Data received.")
             if num_bytes == 0:
                 logger.info(f"Debug message: {data_received.tobytes()}")
             else:
@@ -61,23 +92,25 @@ def run_test(ser):
                     + int(data_received[4]) * 256
                     + int(data_received[5])
                 )
-                logger.info(
-                    f"Bytes received: {num_bytes_received}, "
-                    f"check-sum: {check_sum}"
-                )
                 assert num_bytes_received == data.shape[0] + 2
                 assert data_sum == check_sum
+                i_iter += 1
+                logger.info(f"Test complete.")
                 waiting_for_response = False
 
-    ser.close()
+    t_end = time.time()
+    logger.info(f"Elapsed time: {t_end - t_start:.3f}s for {n_iter} tests.")
+    logger.info(f"Cycle time: {(t_end - t_start) * 1000 / n_iter:.0f}ms.")
 
 
 def main():
-    logger.info(f'{filename} started')
+    logger.info(f'{filename} started.')
     ser = connect()
     logger.info("Connected to Arduino.")
     run_test(ser)
-    logger.info(f"{filename} complete")
+    logger.info(f"{filename} complete.")
+    logger.info('='*35)
+    ser.close()
 
 
 if __name__ == "__main__":
