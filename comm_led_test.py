4d7a6538e97b69f93279efa0455cf8197272d82f
Bill Tubbs <bill.tubbs@me.com>
2025-06-25 21:40:55 -0700

    Started manual test function

--- comm_led_test.py
+++ comm_led_test.py
@@ -1,9 +1,11 @@
 from itertools import cycle
+from collections import deque
 import numpy as np
 import serial
 from serial_comm.serial_comm import (
     connect_to_arduino, send_data_to_arduino, receive_data_from_arduino
 )
+import keyboard
 import logging
 import os
 import time
@@ -31,11 +33,97 @@ SERIAL_PORTS = {
 # Usually, 
 #  - TEENSY1 is on usb port 1275401
 #  - TEENSY2 is on usb port 6862001
-
+port = 50
 
 def connect(address='/dev/cu.usbmodem6862001', baud=BAUD):
-    ser = serial.Serial(address, baud)
+    ser = serial.Serial(SERIAL_PORTS[port], baud)
     return ser
+
+
+def move_pointer_commands(i1, i2):
+    return [
+        np.array(list(b'L1') + [0, i1, 0, 0, 0], dtype="uint8"),
+        np.array(list(b'L1') + [0, i2, 20, 32, 0], dtype="uint8")
+    ]
+    
+
+def manual_testing(ser):
+
+    status, message = connect_to_arduino(ser)
+    if status == 0:
+        worker_name = message
+    else:
+        raise Exception(message)
+    logger.info(f"Hello from: {worker_name}")
+
+    # # Clear display
+    # data = np.array(list(b'LC'), dtype="uint8")
+    # send_data_to_arduino(ser, data)
+
+    # for i in range(50):
+    #     data = np.array(list(b'L1') + [0, i, 32, 4, 32], dtype="uint8")
+    #     send_data_to_arduino(ser, data)
+
+    # # Show LED updates now
+    # np.array(list(b'SN'), dtype="uint8")
+    # send_data_to_arduino(ser, data)
+
+    waiting_for_response = False
+    command_queue = deque()
+    led_id = 0
+    while True:
+
+        # Check for new key presses
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    led_id_new = (led_id - 1) % 100
+                    command_queue.extendleft(
+                        move_pointer_commands(led_id, led_id_new)
+                    )
+                    led_id = led_id_new
+                    logger.info("LEFT key pressed.")
+                if event.key == pygame.K_RIGHT:
+                    led_id_new = (led_id + 1) % 100
+                    command_queue.extendleft(
+                        move_pointer_commands(led_id, led_id_new)
+                    )
+                    led_id = led_id_new
+                    logger.info("RIGHT key pressed.")
+
+        if ser.in_waiting == 0 and waiting_for_response is False:
+            if len(command_queue) > 0:
+                data = command_queue.pop()
+                send_data_to_arduino(ser, data)
+                logger.info("Command sent.")
+                waiting_for_response = True
+
+        if ser.in_waiting > 0:
+            logger.info("Receiving data...")
+            data_received = receive_data_from_arduino(ser)
+            logger.info("Data received.")
+            if np.array_equal(data_received[:2], [0, 0]):
+                # Debug message from Arduino
+                logger.info(f"Debug message: {data_received[2:].tobytes()}")
+            else:
+                # Process data from Arduino - data integrity checks
+                assert data_received.shape[0] == 6
+                num_bytes_received = (
+                    int(data_received[0]) * 256 + int(data_received[1])
+                )
+                data_sum = (
+                    int(data_received[2]) * 16777216
+                    + int(data_received[3]) * 65536
+                    + int(data_received[4]) * 256
+                    + int(data_received[5])
+                )
+                assert num_bytes_received == data.shape[0]
+                assert data_sum == data.sum()
+                logger.info(f"Test {i_iter} complete.")
+                i_iter += 1
+                waiting_for_response = False
+                time.sleep(0.5)
 
 
 def run_test(ser):
@@ -127,7 +215,8 @@ def main():
     logger.info(f'{filename} started.')
     ser = connect()
     logger.info("Connected to Arduino.")
-    run_test(ser)
+    #run_test(ser)
+    manual_testing(ser)
     logger.info(f"{filename} complete.")
     ser.close()
 
