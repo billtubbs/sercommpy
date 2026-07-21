11ae2c67ccfc9d5b6514a48a2b6ed78bbae03d10
Bill Tubbs <bill.tubbs@me.com>
2026-07-20 22:39:22 -0600

    Modified decode test.

--- tests/test_serial_comm.py
+++ tests/test_serial_comm.py
@@ -5,5 +5,9 @@ from serial_comm.serial_comm import encode_data, decode_data
 def test_serial_comm():
     data = np.array([0, 64, 65, 253, 254, 255], dtype="uint8")
     encoded_data = encode_data(data)
+    assert np.array_equal(
+        encoded_data,
+        np.array([0, 64, 65, 252, 253, 0, 253, 1, 253, 2], dtype="uint8"),
+    )
     decoded_data = decode_data(encoded_data)
     assert np.array_equal(decoded_data, data)
