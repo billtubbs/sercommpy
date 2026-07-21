2d752c7bc49aef196e328ce25ddd2d58b0cfb8e4
Bill Tubbs <bill.tubbs@me.com>
2026-07-20 21:36:18 -0700

    Bugfix

--- tests/test_serial_comm.py
+++ tests/test_serial_comm.py
@@ -1,9 +1,9 @@
 import numpy as np
-from serial_comm.serial_comm import encode_data, decode_bytes
+from serial_comm.serial_comm import encode_data, decode_data
 
 
 def test_serial_comm():
-    data = [0, 64, 65, 253, 254, 255]
+    data = np.array([0, 64, 65, 253, 254, 255], dtype="uint8")
     encoded_data = encode_data(data)
-    decoded_data = decode_bytes(encoded_data)
+    decoded_data = decode_data(encoded_data)
     assert np.array_equal(decoded_data, data)
